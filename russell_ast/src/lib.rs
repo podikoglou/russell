3642bc2dc025f7b5fe10a74d8e3f815bcc9b4cce
Alex <alex.podikoglou@gmail.com>
2025-06-07 20:17:32 +0300

    feat(parser): API

--- russell_ast/src/lib.rs
+++ russell_ast/src/lib.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 pub enum ASTNode {
     Variable(char),
     Literal(bool),
--- russell_parser/src/lib.rs
+++ russell_parser/src/lib.rs
@@ -1,14 +1,6 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use russell_ast::ASTNode;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+/// Parses an input into an [ASTNode]
+pub fn parse(input: String) -> anyhow::Result<ASTNode> {
+    todo!()
 }
