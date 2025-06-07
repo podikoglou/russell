5c1a857465e5c537bb2a7e4faeb3909babe070c8
Alex <alex.podikoglou@gmail.com>
2025-06-07 20:55:08 +0300

    test(parser): add tests

--- russell_ast/src/lib.rs
+++ russell_ast/src/lib.rs
@@ -1,4 +1,4 @@
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 pub enum ASTNode {
     Variable(char),
     Literal(bool),
--- russell_parser/src/lib.rs
+++ russell_parser/src/lib.rs
@@ -4,3 +4,121 @@ use russell_ast::ASTNode;
 pub fn parse(input: String) -> anyhow::Result<ASTNode> {
     todo!()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_variables() {
+        for c in 'a'..='z' {
+            assert_eq!(parse(c.to_string()).unwrap(), ASTNode::Variable(c));
+        }
+    }
+
+    #[test]
+    fn test_literals() {
+        assert_eq!(parse("true".to_string()).unwrap(), ASTNode::Literal(true));
+        assert_eq!(parse("false".to_string()).unwrap(), ASTNode::Literal(false));
+    }
+
+    #[test]
+    fn test_paren() {
+        assert_eq!(
+            parse("(true)".to_string()).unwrap(),
+            ASTNode::Paren(Box::new(ASTNode::Literal(true)))
+        );
+        assert_eq!(
+            parse("(false)".to_string()).unwrap(),
+            ASTNode::Paren(Box::new(ASTNode::Literal(false)))
+        );
+
+        assert_eq!(
+            parse("(x)".to_string()).unwrap(),
+            ASTNode::Paren(Box::new(ASTNode::Variable('x')))
+        );
+        assert_eq!(
+            parse("((x))".to_string()).unwrap(),
+            ASTNode::Paren(Box::new(ASTNode::Paren(Box::new(ASTNode::Variable('x')))))
+        );
+    }
+
+    #[test]
+    fn test_not() {
+        assert_eq!(
+            parse("!x".to_string()).unwrap(),
+            ASTNode::Not(Box::new(ASTNode::Variable('x')))
+        );
+
+        assert_eq!(
+            parse("!!x".to_string()).unwrap(),
+            ASTNode::Not(Box::new(ASTNode::Not(Box::new(ASTNode::Variable('x')))))
+        );
+    }
+
+    #[test]
+    fn test_and() {
+        assert_eq!(
+            parse("x && y".to_string()).unwrap(),
+            ASTNode::And(
+                Box::new(ASTNode::Variable('x')),
+                Box::new(ASTNode::Variable('y'))
+            )
+        );
+
+        assert_eq!(
+            parse("x && (y && z)".to_string()).unwrap(),
+            ASTNode::And(
+                Box::new(ASTNode::Variable('x')),
+                Box::new(ASTNode::And(
+                    Box::new(ASTNode::Variable('y')),
+                    Box::new(ASTNode::Variable('z'))
+                ))
+            )
+        );
+    }
+
+    #[test]
+    fn test_or() {
+        assert_eq!(
+            parse("x || y".to_string()).unwrap(),
+            ASTNode::Or(
+                Box::new(ASTNode::Variable('x')),
+                Box::new(ASTNode::Variable('y'))
+            )
+        );
+
+        assert_eq!(
+            parse("x || (y || z)".to_string()).unwrap(),
+            ASTNode::Or(
+                Box::new(ASTNode::Variable('x')),
+                Box::new(ASTNode::Or(
+                    Box::new(ASTNode::Variable('y')),
+                    Box::new(ASTNode::Variable('z'))
+                ))
+            )
+        );
+    }
+
+    #[test]
+    fn test_implies() {
+        assert_eq!(
+            parse("x => y".to_string()).unwrap(),
+            ASTNode::Implies(
+                Box::new(ASTNode::Variable('x')),
+                Box::new(ASTNode::Variable('y'))
+            )
+        );
+
+        assert_eq!(
+            parse("x => (y => z)".to_string()).unwrap(),
+            ASTNode::Implies(
+                Box::new(ASTNode::Variable('x')),
+                Box::new(ASTNode::Implies(
+                    Box::new(ASTNode::Variable('y')),
+                    Box::new(ASTNode::Variable('z'))
+                ))
+            )
+        );
+    }
+}
