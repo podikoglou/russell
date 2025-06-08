f3347358e414971708b6143409c5e1f37c271070
Alex <alex.podikoglou@gmail.com>
2025-06-08 16:28:11 +0300

    feat(engine): implement eval

--- russell_engine/src/lib.rs
+++ russell_engine/src/lib.rs
@@ -1,15 +1,39 @@
+use std::collections::HashMap;
+
+use anyhow::anyhow;
 use russell_ast::ASTNode;
 use russell_parser::parse;
+
+type Assignments = HashMap<char, bool>;
 
 #[derive(Default, Debug)]
 pub struct Engine {}
 
 impl Engine {
-    pub fn eval_str(&self, input: String) -> anyhow::Result<bool> {
-        Ok(self.eval(parse(input)?))
+    pub fn eval_str(&self, input: String, assignments: &Assignments) -> anyhow::Result<bool> {
+        self.eval(parse(input)?, assignments)
     }
 
-    pub fn eval(&self, expr: ASTNode) -> bool {
-        true
+    pub fn eval(&self, expr: ASTNode, assignments: &Assignments) -> anyhow::Result<bool> {
+        match expr {
+            ASTNode::Variable(symbol) => match assignments.get(&symbol) {
+                Some(val) => Ok(*val),
+                None => Err(anyhow!("")),
+            },
+
+            ASTNode::Literal(value) => Ok(value),
+
+            ASTNode::Not(node) => Ok(!self.eval(*node, assignments)?),
+
+            ASTNode::And(p, q) => Ok(self.eval(*p, assignments)? && self.eval(*q, assignments)?),
+
+            ASTNode::Or(p, q) => Ok(self.eval(*p, assignments)? && self.eval(*q, assignments)?),
+
+            ASTNode::Implies(p, q) => {
+                Ok(!self.eval(*p, assignments)? || self.eval(*q, assignments)?)
+            }
+
+            ASTNode::Paren(inner) => self.eval(*inner, assignments),
+        }
     }
 }
