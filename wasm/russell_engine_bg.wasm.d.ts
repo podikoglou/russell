26b2ef689d3c66178eb9df828bd6bc96af209dcb
podikoglou <144563359+podikoglou@users.noreply.github.com>
2025-06-09 13:41:08 +0000

    ci: update wasm build

---
+++ wasm/russell_engine_bg.wasm.d.ts
@@ -0,0 +1,15 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_wasmengine_free: (a: number, b: number) => void;
+export const wasmengine_new: () => number;
+export const wasmengine_eval: (a: number, b: number, c: number, d: any) => [number, number, number];
+export const main: () => void;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_start: () => void;
